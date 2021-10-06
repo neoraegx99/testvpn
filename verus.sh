e0cac0cb217b720da4e13810b1e9f0fc334f1b85
neoraegx99 <79630561+neoraegx99@users.noreply.github.com>
2021-10-06 15:37:42 +0700

    Delete verus.sh

--- verus.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-termux-setup-storage -y
-
-pkg install openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh
-
-./start-ubuntu.sh
